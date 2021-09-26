dabada54c79c0dc76697d48251180f25c83b8206
juancv3d <juancvillarios@hotmail.com>
2021-09-25 21:45:49 -0500

    Creating the item class

--- models/item.py
+++ models/item.py
@@ -1,17 +1,22 @@
+from typing import Dict
 import re
 import requests
+import uuid
 from bs4 import BeautifulSoup
-
-URL = 'https://www.linio.com.co/p/imac-con-pantalla-retina-4k-215-intel-core-i3-1tb-mrt32e-a-apple-jr3hnd?qid=5ed7ee35c9d4a4afb3fe1cf00c2f97f1&oid=AP039EL1L1ECHLCO&position=1&sku=AP039EL1L1ECHLCO'
-TAG_NAME = 'span'
-QUERY = {'class': 'price-main-md'}
+from common.database import Database
 
 
 class Item:
-    def __init__(self, url, tag, query):
+    def __init__(self, url: str, tag: str, query: Dict, _id: str = None):
         self.url = url
         self.tag = tag
         self.query = query
+        self.price = None
+        self.collection = 'items'
+        self._id = _id or uuid.uuid4().hex
+
+    def __repr__(self):
+        return f'<Item {self.url}>'
 
     def get_price(self):
         """
@@ -19,18 +24,33 @@ class Item:
         it will delete the dots and commas from the price to make it easier to convert it to float.
 
         Returns:
-            price (str): The price of the item.
+            price (float): The price of the item.
         """
         response = requests.get(self.url)
         soup = BeautifulSoup(response.content, 'html.parser')
         element = soup.find(self.tag, self.query)
         string_price = element.text.strip()
         pattern = re.compile(r'\d+[\.,]?\d+[\.,]\d+')
-        price = pattern.findall(string_price)[0].replace(
-            ',', '').replace('.', '')
-        return float(price)
+        self.price = float(pattern.findall(string_price)[0].replace(
+            ',', '').replace('.', ''))
+        return self.price
 
+    def save_to_database(self):
+        """
+        Saves the item to the database.
+        """
+        Database.insert(self.collection, self.convert_data_to_json())
 
-if __name__ == '__main__':
-    item = Item(URL, TAG_NAME, QUERY)
-    print(item.get_price())
+    def convert_data_to_json(self):
+        """
+        Converts the data to a json format.
+
+        Returns:
+            json (dict): The data in json format.
+        """
+        return {
+            '_id': self._id,
+            'url': self.url,
+            'tag': self.tag,
+            'query': self.query
+        }
