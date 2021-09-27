e68bc882f315c3695dc8ae03ffe163b67dbebb28
juancv3d <juancvillarios@hotmail.com>
2021-09-27 13:03:05 -0500

    Creating functions to manage basics iteractions

--- common/database.py
+++ common/database.py
@@ -1,7 +1,7 @@
 from typing import Dict
 from dotenv import load_dotenv
 import os
-from pymongo import MongoClient
+from pymongo import MongoClient, cursor
 
 
 load_dotenv()
@@ -14,3 +14,19 @@ class Database:
     @staticmethod
     def insert(collection: str, data: Dict):
         Database.DATABASE[collection].insert(data)
+
+    @staticmethod
+    def find(collection: str, query: Dict) -> cursor:
+        return Database.DATABASE[collection].find(query)
+
+    @staticmethod
+    def find_one(collection: str, query: Dict) -> Dict:
+        return Database.DATABASE[collection].find_one(query)
+
+    @staticmethod
+    def update(collection: str, query: Dict, data: Dict):
+        Database.DATABASE[collection].update(query, data, upsert=True)
+
+    @staticmethod
+    def remove(collection: str, query: Dict):
+        Database.DATABASE[collection].remove(query)
