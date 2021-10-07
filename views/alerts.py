4a5485931d2bfabc834180cfa10942f3824aa88b
juancv3d <juancvillarios@hotmail.com>
2021-10-06 19:04:02 -0500

    Managing how the alerts should work

--- views/alerts.py
+++ views/alerts.py
@@ -1,22 +1,30 @@
-from flask import Blueprint, render_template, request
+import json
+from flask import Blueprint, render_template, request, redirect, url_for
 from models.alert import Alert
+from models.store import Store
+from models.item import Item
+
+alert_blueprint = Blueprint('alerts', __name__)
 
 
-alerts_blueprint = Blueprint('alerts', __name__, template_folder='templates')
-
-
-@alerts_blueprint.route('/')
+@alert_blueprint.route('/')
 def index():
     alerts = Alert.all()
     return render_template('alerts/index.html', alerts=alerts)
 
 
-@alerts_blueprint.route('/new', methods=['GET', 'POST'])
-def new_alert():
+@alert_blueprint.route('/new', methods=['GET', 'POST'])
+def create_alert():
     if request.method == 'POST':
-        item_id = request.form['item_id']
+        item_url = request.form['item_url']
+
+        store = Store.find_by_url(item_url)
+        item = Item(item_url, store.tag_name, store.query)
+        item.save_to_mongo()
+
         price_limit = request.form['price_limit']
 
-        Alert(item_id, price_limit)
+        Alert(item._id, price_limit).save_to_mongo()
 
-    return render_template('alerts/new_alert.html')
+    # What happens if it's a GET request
+    return render_template("alerts/new_alert.html")
